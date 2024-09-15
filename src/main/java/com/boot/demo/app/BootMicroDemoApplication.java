a2c51b27502bb64a1840544de71c2f21c922b327
Ashishyadav9211 <ashishkumarxpt@gmail.com>
2024-09-15 18:18:35 +0530

    sekeleton code commited

---
+++ src/main/java/com/boot/demo/app/BootMicroDemoApplication.java
@@ -0,0 +1,13 @@
+package com.boot.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMicroDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMicroDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/demo/app/BootMicroDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.demo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMicroDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
